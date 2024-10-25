a3f32b0aec16d3fe832c8005854f2c7e3972b6d2
aairu <a>
2024-10-25 16:21:46 +0900

    robot

--- shred/Assets/script/wind.cs
+++ shred/Assets/script/wind.cs
@@ -2,18 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class wind : MonoBehaviour
 {
     public float coefficient;   // ‹ó‹C’ïRŒW”
-    public Vector3 velocity;    // •—‘¬
+     Vector3 velocity;    // •—‘¬
 
+    //•—Œü‚«
+    Vector3 Left=new Vector3(-5,0,0);
+    Vector3 Right=new Vector3(5,0,0);
+    Vector3 Up=new Vector3(0,5,0);
+    Vector3 Down=new Vector3(0,-5,0);
+
+    [SerializeField, Header("Player")]
+    GameObject Player;
+
+    Transform P_t;
     Rigidbody rig;
 
     private void Start()
     {
+        velocity = Vector3.zero;
+        P_t=Player.GetComponent<Transform>();    
+    }
+
+    private void Update()
+    {
+        Debug.Log(velocity);
+        gameObject.transform.position = P_t.position;
     }
     void OnTriggerStay(Collider col)
     {
+        
         Rigidbody rig;
         rig = col.GetComponent<Rigidbody>();
 
@@ -21,9 +41,18 @@ public class wind : MonoBehaviour
         {
             return;
         }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        { velocity = Left; }
+        if (Input.GetKey(KeyCode.RightArrow))
+        { velocity = Right; }
+        if (Input.GetKey(KeyCode.UpArrow))
+        { velocity = Up; }
+        if (Input.GetKey(KeyCode.DownArrow))
+        { velocity = Down; }
 
         // ‘Š‘Î‘¬“xŒvZ
         var relativeVelocity = velocity - rig.velocity;
+
 
         // ‹ó‹C’ïR‚ğ—^‚¦‚é
         rig.AddForce(coefficient * relativeVelocity);
