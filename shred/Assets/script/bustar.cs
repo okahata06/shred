b61a1da5a81b8f1639f1b598fcfbcac325bebc2f
aairu <a>
2024-11-12 12:24:47 +0900

    uuu

--- shred/Assets/script/bustar.cs
+++ shred/Assets/script/bustar.cs
@@ -10,6 +10,7 @@ public class bustar : MonoBehaviour
     float bust_power=0.1f;
 
     Rigidbody rig;
+    //GameObject Hip;
 
     Quaternion quaternion;
     Vector3 set;
@@ -18,18 +19,29 @@ public class bustar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       quaternion.z = gameObject.transform.rotation.z+90;
+        //Hip = transform.parent.gameObject;
+        quaternion.z = gameObject.transform.rotation.z+90;
         set = new Vector3(0, bust_power, 0);
+        rig = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyUp(KeyCode.LeftArrow)) { set.x = 10; }
+        else
+        {
+            set.x = 0;
+        }
+        if(Input.GetKeyUp(KeyCode.RightArrow)) { set.x = -10; }
+        else 
+        {
+            set.x = 0;
+        }
         bust = Vector3.Normalize( quaternion.z * set);
         if(Input.GetKey(KeyCode.Space))
         {
-
-            transform.position += bust;
+            rig.velocity += bust;
         }
     }
 }
