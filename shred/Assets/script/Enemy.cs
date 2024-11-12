54a068e1ab63fe8cebad7c1d35e4f18abd241e3c
岡畑藍 <airuo0603@gmail.com>
2024-11-12 19:05:20 +0900

    下に弾でないように

--- shred/Assets/script/Enemy.cs
+++ shred/Assets/script/Enemy.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField, Header("Player")]
+    GameObject Player;
+
     [SerializeField, Header("弾")]
     GameObject Bullet;
 
+    Transform P_t;
     Quaternion BulletRot;
     int Go_count = 0;
 
@@ -14,17 +18,24 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        BulletRot=gameObject.transform.rotation;
+        Player = GameObject.Find("Hips");
+        P_t = Player.transform;
+        //弾の回転
+        BulletRot = gameObject.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Go_count>=180)
+        //一定間隔＆プレイヤーの座標が下側じゃないとき
+        if(Go_count>=180&&P_t.position.y-transform.position.y>=-1)
         {
+            //インスタンス
             GameObject E_Bullet = Instantiate(Bullet, gameObject.transform.position, BulletRot);
+            //クールタイムリセット
             Go_count = 0;
         }
+        //180までカウント
         Go_count++;
     }
 }
--- shred/Assets/script/EnemyBullet.cs
+++ shred/Assets/script/EnemyBullet.cs
@@ -10,25 +10,33 @@ public class EnemyBullet : MonoBehaviour
     [SerializeField, Header("弾速")]
     float Bullet_Speed = 0.01f;
 
-    int bullet_C=0;
+    int bullet_C=0;//消滅までのカウント
+
     Vector3 Bullet_vec;//弾の放出ベクトル
 
     // Start is called before the first frame update
     void Start()
     {
+        //プレイヤーの座標取得
         Player_t = GameObject.Find("Hips").transform;
+
+        //プレイヤー方向のベクトルを取得
         Bullet_vec.x = Player_t.position.x-gameObject.transform.position.x;
         Bullet_vec.y = Player_t.position.y-gameObject.transform.position.y;
-        Bullet_vec =Vector3.Normalize(Bullet_vec);
+        //正規化
+        Bullet_vec = Vector3.Normalize(Bullet_vec);
 
-
+        //ベクトル方向に弾速を付与
         Bullet_vec = Bullet_vec * Bullet_Speed;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        //ベクトル方向に移動
         transform.position += Bullet_vec;
+        
+        //消滅カウント
         bullet_C++;
         if (bullet_C >= 3000) { Destroy(gameObject); }
     }
