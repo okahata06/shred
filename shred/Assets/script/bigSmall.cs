11265378b5159b9ad33c2da9870aec776c651edd
aairu <a>
2024-11-14 12:27:50 +0900

    getsetなんで

--- shred/Assets/script/bigSmall.cs
+++ shred/Assets/script/bigSmall.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class bigSmall : MonoBehaviour
 {
+
+    bool hit=false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.gameObject.CompareTag("Player"))
+        {
+            //Destroy(this.gameObject);
+            hit= true;
+        }
+        Debug.Log("big"+hit);
+    }
+
+    public bool HitGetSet
+    {
+        get { return hit; }
+        set { hit = value; }
     }
 }
--- shred/Assets/script/camera.cs
+++ shred/Assets/script/camera.cs
@@ -6,9 +6,16 @@ using UnityEngine;
 public class camera : MonoBehaviour
 {
     [SerializeField,Header("í èÌzç¿ïW")]
-    int C_posZ = -20;
+    float C_posZ = -20;
     [SerializeField,Header("í«ê’ëŒè€")]
     public GameObject target;
+
+    [SerializeField, Header("ägèkó ")]
+    float MaxMove = 1;
+
+    float move = 0.01f;
+
+    bigSmall BigSmall;
 
     Transform c_transform;
 
@@ -17,12 +24,19 @@ public class camera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        BigSmall=new bigSmall();
+        MaxMove += C_posZ;
         c_transform= GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(BigSmall.HitGetSet);
+        if(BigSmall.HitGetSet&&C_posZ<=MaxMove)
+        {
+            C_posZ += move;
+        }
 
         c_transform.position = new Vector3(target.transform.position.x, target.transform.position.y+5, C_posZ);
 
