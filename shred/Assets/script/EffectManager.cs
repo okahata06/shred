970b2f43846d388f5d9d2d4dabcf4dec896a32a7
aairu <a>
2024-11-21 13:54:57 +0900

    ee

---
+++ shred/Assets/script/EffectManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour
+{
+
+    [SerializeField, Header("爆発エフェクト")]
+    GameObject[] effect;
+
+    int E_volume=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        E_volume = effect.Length;
+    }
+
+    // Update is called once per frame
+    public GameObject GetSet1Effect
+    {
+        get {  return effect[0]; } 
+    }
+}
