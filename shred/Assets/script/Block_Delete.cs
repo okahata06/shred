acb3b379623c4e28b00323ba0e88df3849c3c67d
岡畑藍 <airuo0603@gmail.com>
2025-09-24 15:39:23 +0900

    削除時間を８に変更

--- shred/Assets/script/Block_Delete.cs
+++ shred/Assets/script/Block_Delete.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Block_Delete : MonoBehaviour
 {
-    int deleteTime = 15;
+    int deleteTime = 8;
     float time;
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
         if(time > deleteTime)
+        {
+            Debug.Log(time);
             Destroy(gameObject);
+
+        }
     }
 }
